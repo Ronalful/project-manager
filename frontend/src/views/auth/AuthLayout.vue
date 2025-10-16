77555f7ad4e0f728d92cb4b6b55f27ce9603e1b0
Blank36 <nikolaenkoyu36@gmail.com>
2025-10-16 10:36:36 +0300

    исправлены стили, свитчер помещен в шаблон

--- frontend/src/views/auth/AuthLayout.vue
+++ frontend/src/views/auth/AuthLayout.vue
@@ -1,4 +1,9 @@
+<script setup>
+import ThemeSwitch from "@/components/ThemeSwitch.vue";
+</script>
+
 <template>
+  <ThemeSwitch></ThemeSwitch>
   <div class="auth-layout">
     <div class="auth-container">
       <router-view /> <!-- Здесь будут рендериться дочерние компоненты -->
